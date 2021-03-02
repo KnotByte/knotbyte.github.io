0677db29078fbbbfc996dec015b425027b5fed15
joynahiid <nahidhasan282@gmail.com>
2021-03-03 05:37:45 +0600

    Initialzing...

---
+++ js/joy.js
@@ -0,0 +1,12 @@
+$("body").flowtype({
+  minimum: 400,
+  maximum: 1200,
+  minFont: 12,
+  maxFont: 18,
+  fontRatio: 30,
+});
+
+tippy("#fb", {
+  content: "fb.me/knotbyte",
+  animation: "scale",
+});
